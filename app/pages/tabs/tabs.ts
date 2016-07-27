e4a11974ed5cd9461fb459d5b605a078f54f81b2
xiaomo <83387856@qq.com>
2016-07-27 15:02:04 +0800

    添加了一个页面

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,22 +1,25 @@
-import {Component} from '@angular/core';
-import {HomePage} from '../home/home';
-import {AboutPage} from '../about/about';
-import {ContactPage} from '../contact/contact';
+import { Component } from "@angular/core";
+import { HomePage } from "../home/home";
+import { AboutPage } from "../about/about";
+import { ContactPage } from "../contact/contact";
+import { UsePage } from "../use/use";
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
 })
 export class TabsPage {
 
-  private tab1Root: any;
-  private tab2Root: any;
-  private tab3Root: any;
+  private home: any;
+  private concat: any;
+  private use: any;
+  private about: any;
 
   constructor() {
     // this tells the tabs component which Pages
     // should be each tab's root Page
-    this.tab1Root = HomePage;
-    this.tab2Root = AboutPage;
-    this.tab3Root = ContactPage;
+    this.home = HomePage;
+    this.concat = ContactPage;
+    this.use = UsePage;
+    this.about = AboutPage;
   }
 }
---
+++ app/pages/use/use.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/use/use.html'
+})
+export class UsePage {
+  constructor(private navCtrl: NavController) {
+  }
+}
